3883833c78b44018418dd5521aa8f8264a6c2fdb
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-07-19 17:27:18 +0200

    Update HTML footer via JS

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("Witaj wędrowcze...")
+
+let lastUpdate = document.querySelector('.footer__date--js')
+lastUpdate.innerHTML = 'Ostatnia aktualizacja: 07.2023'
+console.log(lastUpdate.innerHTML)
