74cc7bad9fe8a4ca852e0cd9df45863fe6326298
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-07-31 15:39:41 +0200

    Add js hello function

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ console.log("Witaj wędrowcze...")
 let lastUpdate = document.querySelector('.footer__date--js')
 lastUpdate.innerHTML = 'Ostatnia aktualizacja: 07.2023'
 console.log(lastUpdate.innerHTML)
+
+function hello(name, age) {
+  return `Witaj ${name}. Twój wiek to ${age}.`
+}
+
+console.log(hello("Janek", "20"))
+console.log(hello("Ewa", "22"))
+console.log(hello("Karol", "24"))
