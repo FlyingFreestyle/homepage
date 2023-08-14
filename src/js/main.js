7c3761bffe7b406a72bf75b2655e97db3d8255bb
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-08-14 18:28:50 +0200

    Add substract function, use in main

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import { hello } from "./hello"
+import { substract } from "./substract"
 
 console.log("Witaj wędrowcze...")
 
 console.log(hello("Janek", "20"))
 console.log(hello("Ewa", "22"))
 console.log(hello("Karol", "24"))
+
+console.log(substract(25, 11))
