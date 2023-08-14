7ab3a88ce6d7ce2fa92ccd7e21055eb84032e544
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-08-14 13:47:31 +0200

    Import date-fns to footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,2 +1,9 @@
+import format from 'date-fns/format'
+import pl from "date-fns/locale/pl"
+
+const stamp = format(new Date(2023, 7, 14), "LLLL yyyy", {
+    locale: pl
+})
+
 let lastUpdate = document.querySelector('.footer__date--js')
-lastUpdate.innerHTML = 'Ostatnia aktualizacja: 08.2023'
+lastUpdate.innerHTML = `Ostatnia aktualizacja: ${stamp}`
