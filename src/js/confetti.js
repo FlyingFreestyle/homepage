14c47b4aad6a3d7cde387fa6627bab7a6d3400c4
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-09-06 18:20:07 +0200

    Add confetti effect when hour equals minute

---
+++ src/js/confetti.js
@@ -0,0 +1,17 @@
+import JSConfetti from "js-confetti";
+const jsConfetti = new JSConfetti()
+
+
+function checkHourEqualsMinute() {
+    const now = new Date();
+    const currentHour = now.getHours();
+    console.log(currentHour)
+    const currentMinute = now.getMinutes();
+
+    if (currentHour === currentMinute) {
+      jsConfetti.addConfetti()
+    }
+}
+
+console.log("setting up confetti")
+setInterval(checkHourEqualsMinute, 1000*10);
